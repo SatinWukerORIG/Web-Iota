fb827215d83242791f7557ae9d0cf7a8b55c7270
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-12-16 11:39:28 -0800

    Add new classes, change webdriver
    Helper class is added, which contains helper functions that perform numerous trivial tasks
    Since PhantomJS is deprecated by selenium, I replaced it with the Chromedriver.
    webdriver_manager is also added to provide an optimized installation for the users, so that they do not need to downloadchromedrivers on from https://chromedriver.chromium.org/downloads

--- iota2.py
+++ iota2.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from argparse import ArgumentParser
 
@@ -11,82 +14,98 @@ Step 2. Find suburls or links of the web page
 Step 3. Add raw links/urls to a list
 Step 4. Remove duplicates of the list and output
 """
-
-driver = webdriver.PhantomJS()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 download_img = False
 
-def decoration(output):
-    print('\n', output, '\n', '--' * 20)
+class LOG:
+    def warn(msg):
+        print("warning: " + msg)
+    
+    def error(msg):
+        print("error: " + msg)
+    
+    def info(msg):
+        print("info: " + msg)
 
+    def decoration(output):
+        print('\n', output, '\n', '--' * 20)
 
-def remove_url_ends(url):
-    if 'https' in url:
-        return 'https://' + url.split('//')[1].split('/')[0]
-    return 'http://' + url.split('//')[1].split('/')[0]
+class HELPER:
 
-def download_image(name, url):
-    with open(name, 'wb') as f:
-        f.write(requests.get(url).content)
+    def if_url(url):
+        return ('http' or 'www.') in url
 
-def get_html(parent_url):
-    # head = {
-    #     "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-    # }
-    driver.get(parent_url)
-    soup = BeautifulSoup(str(driver.page_source), 'html.parser')
-    return soup
-
-# Find suburls or links of the web page
-def find_links(parent_url):
-    url_list = []
-    url_prefix = remove_url_ends(parent_url)
-    for link in get_html(parent_url).find_all('a'):
+    def remove_url_ends(url):
         try:
-            url_list.append(link.get('href') if 'http' in link.get('href') else url_prefix + link.get('href'))
+            if 'https' in url:
+                return 'https://' + url.split('//')[1].split('/')[0]
+            return 'http://' + url.split('//')[1].split('/')[0]
         except:
-            continue
+            print("please input the full url")
 
-    decoration('Links:')
-    url_list = list(set(url_list))
-    for i in range(len(url_list)):
-        print(f'[{i + 1}]{url_list[i]}')
+    def download_image(name, url):
+        with open(name, 'wb') as f:
+            f.write(requests.get(url).content)
+
+    def get_html(parent_url):
+        driver.get(parent_url)
+        soup = BeautifulSoup(str(driver.page_source), 'html.parser')
+        return soup
 
 
-def find_img(parent_url):
-    url_list = []
-    url_prefix = remove_url_ends(parent_url)
-    html = get_html(parent_url)
-    for link in html.find_all('img'):
-        url_list.append(link.get('src') if 'http' in link.get('src') else url_prefix + link.get('src'))
+class Instructions(HELPER):
+    def find_files(self, parent_url):
+        LOG.decoration('Files:')
+        url_list = []
+        suffix = input('file suffix: ')
+        for x in find_links(parent_url):
+            if suffix in x:
+                print(x)
+                url_list.append(x)
 
-    url_list = list(set(url_list))
+        if len(url_list) == 0:
+            print("Couldn't find anything...")
 
-    decoration('Images:')
-    for i in range(len(url_list)):
-        print(f'[{i + 1}]{url_list[i]}')
-        if download_img:
+    # Find suburls or links of the web page
+    def find_links(self, parent_url):
+        url_list = []
+        url_prefix = HELPER.remove_url_ends(parent_url)
+        for link in HELPER.get_html(parent_url).find_all('a'):
             try:
-                download_image(f'img[{i + 1}].png', url_list[i])
-                print('download successfully...')
-            except:
-                print('invalid image...')
+                href = link.get('href')
+                if if_url(href):
+                    url_list.append(href)
+                else:
+                    url_list.append(url_prefix + href)
+            except Exception as e:
+                LOG.warn(e)
 
+        return list(set(url_list))
 
-def find_all_img(url):
-    pass
+    def find_img(self, parent_url):
+        url_list = []
+        url_prefix = HELPER.remove_url_ends(parent_url) # http||https
+        html = HELPER.get_html(parent_url) # soup(html)
 
-def find_files(parent_url):
-    decoration('Files:')
-    url_list = []
-    suffix = input('file suffix: ')
-    for x in find_links(parent_url):
-        if suffix in x:
-            print(x)
-            url_list.append(x)
+        for link in html.find_all('img'):
+            src = link.get('src')
+            if HELPER.if_url(src):
+                url_list.append(src)
+            else:
+                url_list.append(url_prefix + src)
 
-    if len(url_list) == 0:
-        print("Couldn't find anything...")
+        return list(set(url_list))
+
+    def find_all_img(self, url):
+        pass
+
+    def find_all_links(self, url):
+        print(find_links(url))
 
 
 def main():
@@ -95,21 +114,39 @@ def main():
     parser.add_argument("url", nargs="?", default="", help='The URL of the target website/webpage')
     parser.add_argument('-img', help='Find all of the image on the webpage', action='store_true')
     parser.add_argument('-all_img', help='Find all of the image on the webpage and subwebpages', action='store_true')
-    parser.add_argument('-link', help='Find all of the suburls/links on the webpage', action='store_true')
+    parser.add_argument('-link', help='Find the suburls/links on the webpage', action='store_true')
+    parser.add_argument('-all_links', help='Find all of the suburls/links on the webpage', action='store_true')
     parser.add_argument('--download', help='Download images', action='store_true')
     args = parser.parse_args()
 
     global download_img
     download_img = args.download
+    INSTRUC = Instructions()
 
     if args.img:
-        find_img(args.url)
+        url_list = INSTRUC.find_img(args.url)
+        LOG.decoration('Images:')
+        for i in range(len(url_list)):
+            print(f'[{i + 1}]{url_list[i]}')
+            if not download_img: continue
+            try:
+                HELPER.download_image(f'img[{i + 1}].png', url_list[i])
+                LOG.info('download successfully...')
+            except:
+                LOG.error('invalid image...')
+
 
     if args.all_img:
-        find_all_img(args.url)
+        INSTRUC.find_all_img(args.url)
 
     if args.link:
-        find_links(args.url)
+        url_list = INSTRUC.find_links(args.url)
+        LOG.decoration('Links:')
+        for i in range(len(url_list)):
+            print(f'[{i + 1}]{url_list[i]}')
+
+    if args.all_links:
+        INSTRUC.find_all_links(args.url)
 
 
 if __name__ == '__main__':
